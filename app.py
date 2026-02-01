31b1ed6e90c04d22794dda6a74b865cc62734754
Lotta Toivanen <lotta.toivanen@helsinki.fi>
2026-02-01 21:13:59 +0200

    Add new entry

--- app.py
+++ app.py
@@ -12,6 +12,18 @@ app.secret_key = config.secret_key
 def index():
     return render_template("index.html")
 
+@app.route("/new_entry", methods=["POST"])
+def new_entry():
+    title = request.form["title"]
+    description = request.form["description"]
+    date = request.form["date"]
+    user_id = session["user_id"]
+
+    sql = "INSERT INTO entries (title, description, date, user_id) VALUES (?, ?, ?, ?)"
+    db.execute(sql, [title, description, date, user_id])
+
+    return redirect("/")
+
 @app.route("/register")
 def register():
     return render_template("register.html")
@@ -40,11 +52,16 @@ def login():
     if request.method == "POST":
         username = request.form["username"]
         password = request.form["password"]
-    
-        sql = "SELECT password_hash FROM users WHERE username = ?"
+
+        sql = "SELECT id, password_hash FROM users WHERE username = ?"
+        result = db.query(sql, [username])[0]
+        user_id = result["id"]
+        password_hash = result["password_hash"]
+
         password_hash = db.query(sql, [username])[0][0]
 
         if check_password_hash(password_hash, password):
+            session["user_id"] = user_id
             session["username"] = username
             return redirect("/")
         else:
@@ -52,5 +69,6 @@ def login():
 
 @app.route("/logout")
 def logout():
+    del session["user_id"]
     del session["username"]
     return redirect("/")
--- schema.sql
+++ schema.sql
@@ -3,3 +3,12 @@ CREATE TABLE users (
     username TEXT UNIQUE,
     password_hash TEXT
 );
+
+CREATE TABLE entries (
+    id INTEGER PRIMARY KEY,
+    title TEXT,
+    description TEXT,
+    date DATE
+    user_id INTEGER REFERENCES users
+    
+);
